c811da872031ac2e0aae99901cce27f435b9c82e
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2025-03-20 17:18:56 +0900

    `ThemeMode` に関するコードを削除
    `MainWindow.xaml.cs` のコンストラクタから、`ThemeMode` に関連するコメントとメッセージングの登録コードを削除しました。これにより、クラスの初期化が簡素化されました。

--- FluentDemo/MainWindow.xaml.cs
+++ FluentDemo/MainWindow.xaml.cs
@@ -13,12 +13,5 @@ public partial class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
-
-		// ThemeMode は Binding 可能なプロパティではないので、View で設定する必要がある
-		WeakReferenceMessenger.Default.Register<MainWindow, PropertyChangedMessage<ThemeMode>>(this, static (window, message) =>
-		{
-			Debug.WriteLine($"ThemeModeChanging:{message.OldValue} -> {message.NewValue}");
-			window.ThemeMode = message.NewValue;
-		});
 	}
 }
